c10ae7487ae689ee1f4666ba25b229785a4bddb0
Rudrh Nair <rudrh.nair@gmail.com>
2026-09-12 20:08:00 -0400

    add ECE option and update AP/ECE weighting check

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function calculateGPA() {
   for (let i = 0; i < classes.length; i++) {
     total += gradeToPoints(Number(classes[i].grade));
     if (classes[i].type === "Honors") honorsCount++;
-    if (classes[i].type === "AP") apCount++;
+    if (classes[i].type === "AP" || classes[i].type === "ECE") apCount++;
   }
 
   let unweighted = total / classes.length;
